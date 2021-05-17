dd1223609795a1c90ba22db5e7988a81ed24ea9b
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-05-17 16:58:22 -0300

    refactremove method create in IUserRepository

--- src/data/Repositories/IUsersRepository.ts
+++ src/data/Repositories/IUsersRepository.ts
@@ -2,7 +2,6 @@ import { User } from '../../domain/entities/User';
 
 export interface IUsersRepository {
   findByCpf(cpf: string): Promise<User>;
-  save(user: User): Promise<void>;
-  create(): User;
+  save(user: User): Promise<User>;
   findById(id: string): Promise<User>;
 }
