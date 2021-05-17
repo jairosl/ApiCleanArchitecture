4c53eb9f9a2074c6217cc698f76c5ecd276b6e23
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-05-17 17:00:10 -0300

    feat: create userController

---
+++ src/domain/useCases/createUserUseCase/index.ts
@@ -0,0 +1,11 @@
+import { PostgresUserRepository } from '../../../data/Repositories/implements/PostgresUserRepository';
+import { CreateUserController } from './CreateUserController';
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+const postgresUserRepository = new PostgresUserRepository();
+
+const createUserUseCase = new CreateUserUseCase(postgresUserRepository);
+
+const createUserController = new CreateUserController(createUserUseCase);
+
+export { createUserController };
