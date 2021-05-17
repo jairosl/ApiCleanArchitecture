40575d6883f02066fa93683030205f6e66ecfffa
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-05-17 16:54:50 -0300

    feat: add routes in app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'reflect-metadata';
 import { PostgresDatabase } from './infra/db/Postgres/connection';
+import { router } from './routes';
 
 const pg = new PostgresDatabase();
 
@@ -9,5 +10,6 @@ pg.connect();
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
 export { app };
