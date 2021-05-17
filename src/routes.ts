774424a5cf4463ff6ca77bfcfa0745553dc36b56
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-05-17 16:55:46 -0300

    feat:create method post for users

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,8 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { createUserController } from './domain/useCases/createUserUseCase';
 
 const router = Router();
+
+router.post('/users', (request: Request, response: Response) => createUserController.handle(request, response));
 
 export { router };
