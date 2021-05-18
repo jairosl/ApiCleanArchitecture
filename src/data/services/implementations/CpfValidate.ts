b792a389e65af23a5e15f590f60cab7d1e524019
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-05-18 13:07:44 -0300

    refactor: remove valid format in field cpf

--- src/data/services/implementations/CpfValidate.ts
+++ src/data/services/implementations/CpfValidate.ts
@@ -4,11 +4,8 @@ import { IValidateCpf } from '../IValidateCpf';
 
 export class CpfValidate implements IValidateCpf {
   validate(cpf: string) {
-    const regex = /([0-9]{2}[\.]?[0-9]{3}[\.][0-9]{3}[\/][0-9]{4}[-][0-9]{2})|([0-9]{3}[\.][0-9]{3}[\.][0-9]{3}[-][0-9]{2})/;
-
-    const validateFormat = regex.test(cpf);
     const validateNumber = cpfValidator.isValid(cpf);
 
-    return validateFormat && validateNumber;
+    return validateNumber;
   }
 }
