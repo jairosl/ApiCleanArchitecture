82f67c3d963c499b776a0f864045e4fb27dd7f80
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-05-18 13:42:53 -0300

    refactor: change phone number regex valid

--- src/data/services/implementations/NumberPhoneValidate.ts
+++ src/data/services/implementations/NumberPhoneValidate.ts
@@ -2,7 +2,8 @@ import { IValidateNumberPhone } from '../IValidateNumberPhone';
 
 export class PhoneNumberValidator implements IValidateNumberPhone {
   validate(phone: string) {
-    const regex = /(0?[1-9]{2})*\D*(9?)\D?(\d{4})+\D?(\d{4})\b/;
+    // eslint-disable-next-line no-useless-escape
+    const regex = /^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$/;
     const validateFormat = regex.test(phone);
 
     return validateFormat;
