e6d98085a15823c669d0cd18abf25cb866107944
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-05-17 21:54:05 -0300

    feat: add services validate Cpf

--- src/domain/useCases/createUserUseCase/CreateUserUseCase.ts
+++ src/domain/useCases/createUserUseCase/CreateUserUseCase.ts
@@ -1,12 +1,22 @@
-import { IUsersRepository } from '../../../data/Repositories/IUsersRepository';
+import { IUsersRepository } from '../../../data/repositories/IUsersRepository';
+import { IValidateCpf } from '../../../data/services/IValidateCpf';
 import { ICreateUserRequestDTO, ICreateUserResponseDTO } from './CreateUserDTO';
 
 export class CreateUserUseCase {
   constructor(
     private usersRepository: IUsersRepository,
+    private validateCpf: IValidateCpf,
   ) {}
 
   async execute(data: ICreateUserRequestDTO): Promise<ICreateUserResponseDTO> {
+    if (!this.validateCpf.validate(data.cpf)) {
+      const responseCpfInvalid: ICreateUserResponseDTO = {
+        success: false,
+        message: 'CPF Invalido',
+      };
+      return responseCpfInvalid;
+    }
+
     const userAlreadyExists = await this.usersRepository.findByCpf(data.cpf);
 
     if (userAlreadyExists) {
