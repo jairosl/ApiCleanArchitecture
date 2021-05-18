2626193cae12517d053fd1697acfa952d1c7663c
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-05-18 08:00:26 -0300

    chore: add dotenv, helmet and cros

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,25 @@
 import express from 'express';
 import 'reflect-metadata';
+import cors from 'cors';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
 import 'express-async-errors';
 import { PostgresDatabase } from './infra/db/Postgres/connection';
 import { router } from './routes';
+
+dotenv.config();
 
 const pg = new PostgresDatabase();
 
 pg.connect();
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+}));
+
+app.use(helmet());
 
 app.use(express.json());
 app.use(router);
