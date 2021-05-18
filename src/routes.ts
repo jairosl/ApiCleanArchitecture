09affe97b2606e79a90bfb7dd49096c737156da4
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-05-18 13:32:32 -0300

    feat: add the path cpf in routes with method get

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response } from 'express';
 import { createUserController } from './domain/useCases/createUserUseCase';
+import { verifyCpfController } from './domain/useCases/verifyCpfUserUseCase';
 
 const router = Router();
 
 router.post('/users', (request: Request, response: Response) => createUserController.handle(request, response));
+router.get('/cpf/:cpf', (request: Request, response: Response) => verifyCpfController.handle(request, response));
 
 export { router };
