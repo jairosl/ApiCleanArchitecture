a50871e4e3c7406e10e00bacf4d5a819c9eef90e
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-05-18 10:45:54 -0300

    refactor: use environment variables to connect the database

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 module.exports = {
    "type": "postgres",
-   "host": "localhost",
-   "port": 5432,
-   "username": "postgres",
-   "password": "jairosl10",
-   "database": "testbluelab",
+   "host": process.env.HOST_DB,
+   "port": process.env.PORT_DB,
+   "username": process.env.USERNAME_DB,
+   "password": process.env.PASSWORD_DB,
+   "database": process.env.NAME_DB,
 
   "migrations": ["./src/infra/db/migrations/**.ts"],
   "entities": ["./src/infra/db/Postgres/entities/*.ts"],
